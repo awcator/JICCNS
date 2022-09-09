42256273e1487cf0622e33287fe80ef3351f89d4
awcator <awcator@protonmail.com>
2022-09-09 12:19:13 +0530

    missed meta file

---
+++ jicn_simulator/src/main/java/awcator/jicns/meta.java
@@ -0,0 +1,9 @@
+package awcator.jicns;
+import org.json.*;
+public final class meta {
+    public static final String JICNS_version = "JICNS_202209";
+    public static String blueprint_map="{}";
+    public static String  loadBluePrint(){
+        return "{\"nodes_blueprint\":{\"node_count\":\"10\",\"node_prefix\":\"Node\",\"node_ui_width\":\"70\",\"node_ui_height\":\"50\"}}\n";
+    }
+}
