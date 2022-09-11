c96ff8a6956fc1f3ff88e40b6420bfa2c6f13e8f
awcator <awcator@protonmail.com>
2022-09-11 15:05:38 +0530

    Dummy broadcaster demo

--- jicn_simulator/src/main/java/Main.java
+++ jicn_simulator/src/main/java/Main.java
@@ -2,11 +2,23 @@ import awcator.jicns.meta;
 import awcator.jicns.ui.frame;
 
 public class Main {
-    public static final String version = "JICNS_202209";
+    public static final String version = "JICNCS_202209";
+    /**
+     * public static int[][] a = {{-1, 3, 2, -1, -1, -1}, //0
+     *             {-1, -1, -1, -1, 2, -1}, //1
+     *             {-1, -1, -1, -1, -1, 1}, //2
+     *             {4, -1, 5, -1, -1, -1}, //3
+     *             {1, -1, 1, -1, -1, 8},//4
+     *             {-1, -1, -1, 1, -1, -1} //5
+     *     };
+     * A simple graph i used to test the environemt
+    */
+    public static int ms = 0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Loading " + version);
         meta.blueprint_map = meta.loadBluePrint();
         frame app = new frame();
     }
 }
+
